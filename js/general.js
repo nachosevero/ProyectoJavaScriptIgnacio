2f2e3cb1e3f40f1b6c7dbcfbdf65251c84f78baf
ignacioperez <nachosevero23@gmail.com>
2023-08-03 13:51:58 -0300

    Inicio git

---
+++ js/general.js
@@ -0,0 +1,70 @@
+const Productos = [
+    {
+        imagen:"./img/graficanvidia.jpg", 
+        id: 1, 
+        titulo:"GPU Nvidia", 
+        precio: 28275
+    },
+    {
+        imagen:"./img/graficaamd.jpg", 
+        id: 2, 
+        titulo:"GPU Amd", 
+        precio: 60255
+    },
+    {
+        imagen:"./img/placaasus.jpg", 
+        id: 3, 
+        titulo:"Mother Asus", 
+        precio: 26910
+    },
+    {
+        imagen:"./img/placamadreasrock.jpg", 
+        id: 4, 
+        titulo:"Mother Asrock", 
+        precio: 33150
+    },
+    {
+        imagen:"./img/ram.jpg", 
+        id: 5, 
+        titulo:"RAM Kingston", 
+        precio: 4290
+    },
+    {
+        imagen:"./img/ssd.jpg", 
+        id: 6, 
+        titulo:"SSD Kingston", 
+        precio: 11505
+    },
+    {
+        imagen:"./img/fuentecoolermaster.jpg", 
+        id: 7, 
+        titulo:"Fuente Cooler Master", 
+        precio: 17550
+    },
+    {
+        imagen:"./img/monitor.jpg", 
+        id: 8, 
+        titulo:"Monitor Viewsonic", 
+        precio: 19110
+    },
+    {
+        imagen:"./img/teclado.jpeg", 
+        id: 9, 
+        titulo:"Teclado Logitech", 
+        precio: 11115
+    },
+    {
+        imagen:"./img/mouse.jpg", 
+        id: 10, 
+        titulo:"Mouse Razer", 
+        precio: 4875
+    },
+    {
+        imagen:"./img/auriculares.jpg", 
+        id: 11, 
+        titulo:"Auriculares Hyperx", 
+        precio: 8346
+    }
+]
+
+const carritoProductos = []
---
+++ js/index.js
@@ -0,0 +1,56 @@
+// DOM
+const contenedorProductos = document.querySelector("div#contenedor-productos.contenedor-productos")
+const inputBuscar = document.querySelector("input#input-buscar")
+
+function retornarProductoHTML(producto) {
+    return `<div class="producto">
+                <span class="producto-titulo">${producto.titulo}</span>
+                <img src="${producto.imagen}" alt="${producto.titulo}" class="producto-imagen">
+                <span class="producto-precio">$${producto.precio}</span>
+                <button class="producto-agregar" id="${producto.id}">Agregar al carrito</button>
+            </div> `
+}
+
+function activarButtonAgregar() {
+    const botones = document.querySelectorAll("button.producto-agregar")
+    botones.forEach((boton)=> {
+        boton.addEventListener("click", ()=>{
+            let producto = Productos.find((producto)=> producto.id === parseInt(boton.id))
+            carritoProductos.push(producto)
+            console.table(carritoProductos)
+        })
+    })
+}
+
+//Cargar los productos
+
+function cargarProductos(array) {
+    contenedorProductos.innerHTML = ""
+    array.forEach((producto)=> {
+        contenedorProductos.innerHTML += retornarProductoHTML(producto)
+    })
+    activarButtonAgregar()
+}
+cargarProductos(Productos)
+
+//Buscador
+
+inputBuscar.addEventListener("search", ()=>  {
+        const resultado = Productos.filter((producto)=> producto.titulo.toLowerCase().includes(inputBuscar.value.toLowerCase()))
+        cargarProductos(resultado)
+        
+})
+
+
+
+
+
+/*
+<div class="producto">
+<span class="producto-titulo">GPU Nvidia</span>
+<img src="./img/graficanvidia.jpg" alt="" class="producto-imagen">
+<span class="producto-precio">$28275</span>
+<button class="producto-agregar">Agregar al carrito</button>
+</div>
+
+*/ 
